d031765227d9ddf909f20aee4a994d316647e67b
jayanth <jayanths8899@gmail.com>
2024-07-05 20:17:24 +0530

    Changes

--- getlatlong.py
+++ getlatlong.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify, request
 
 from google.cloud import firestore
@@ -35,4 +36,4 @@ def get_coordinates_for_orders():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
